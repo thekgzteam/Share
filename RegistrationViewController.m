bd1529eee1f1c79f63c9ce776d5a1c799c7befbe
Edil Ashimov <edilashimov@Edils-MacBook-Pro.local>
2015-08-17 17:29:48 -0500

    parse frameworks

--- RegistrationViewController.m
+++ RegistrationViewController.m
@@ -9,6 +9,7 @@
 #import "RegistrationViewController.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <Parse/Parse.h>
 @interface RegistrationViewController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextfield;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextfield;
@@ -23,7 +24,22 @@
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
     loginButton.center = CGPointMake(160.0, 380.0);
     [self.view addSubview:loginButton];
-    
+    PFUser *user = [PFUser user];
+    user.username = @"my name";
+    user.password = @"my pass";
+    user.email = @"email@example.com";
+
+    // other fields can be set if you want to save more information
+    user[@"phone"] = @"650-555-0000";
+
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            // Hooray! Let them use the app now.
+        } else {
+            NSString *errorString = [error userInfo][@"error"];
+            // Show the errorString somewhere and let the user try again.
+        }
+    }];
 //
 //
 //        self.usernameTextfield.text = @"PlaceholderText";
