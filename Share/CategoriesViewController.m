ae2bc2d2f2ff73ed8b197a514aa2ed2f5fc2f715
Jim Witheril <jwitheril@austin.rr.com>
2015-08-17 17:30:37 -0500

    changes

--- Share/CategoriesViewController.m
+++ Share/CategoriesViewController.m
@@ -12,26 +12,37 @@
 
 @end
 
-@implementation CategoriesViewController
+@implementation
+
+CategoriesViewController
+- (IBAction)buttonRestaurants:(UIButton *)sender {
+
+}
+
+- (IBAction)buttonEntertainment:(UIButton *)sender {
+}
+- (IBAction)buttonNews:(UIButton *)sender {
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
